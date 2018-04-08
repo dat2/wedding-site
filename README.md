bae3d9bbb52008baa4939aa4b2d32198e542e2b3
Nicholas Dujay <nickdujay@gmail.com>
2018-04-08 16:09:18 -0400

    clean up a bit

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# gatsby-starter-default
-The default Gatsby starter.
+# wedding-site
+The source for nick.pirave.com
 
 For an overview of the project structure please refer to the [Gatsby documentation - Building with Components](https://www.gatsbyjs.org/docs/building-with-components/).
 
@@ -9,18 +9,3 @@ Make sure that you have the Gatsby CLI program installed:
 ```sh
 npm install --global gatsby-cli
 ```
-
-And run from your CLI:
-```sh
-gatsby new gatsby-example-site
-```
-
-Then you can run it by:
-```sh
-cd gatsby-example-site
-npm run develop
-```
-
-## Deploy
-
-[![Deploy to Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/gatsbyjs/gatsby-starter-default)
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,33 +1,35 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  background: rebeccapurple;
+  margin-bottom: 1.45rem;
+`
+
+const Centered = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+`
+
+const H1 = styled.h1`
+  margin: 0;
+`
+
+const StyledLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+`
 
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
-  </div>
+  <Container>
+    <Centered>
+      <H1>
+        <StyledLink to="/">Nick Weds Pirave</StyledLink>
+      </H1>
+    </Centered>
+  </Container>
 )
 
 export default Header
