9342f8b40fb9ee81c9da8ef85d6fb0c3537da16b
Nicholas Dujay <nickdujay@gmail.com>
2018-04-08 16:15:14 -0400

    layout changes

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,30 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import styled from 'styled-components'
 
-import Header from '../components/Header'
 import './index.css'
+import Header from '../components/Header'
+
+const Container = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0px 1.0875rem 1.45rem;
+  padding-top: 0;
+`
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="Nick Weds Pirave"
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: 'Nick and Pirave wedding website' },
+        { name: 'keywords', content: 'nick, pirave, wedding' },
       ]}
     />
     <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+    <Container>{children()}</Container>
   </div>
 )
 
