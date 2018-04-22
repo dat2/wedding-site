6876340b4419b06600245c01e60bff285f6e52c0
Nicholas Dujay <nickdujay@gmail.com>
2018-04-22 14:43:43 -0400

    cool change small things

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
+import { formatDistance } from 'date-fns'
 
-const IndexPage = () => <div />
+const Centered = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`
+
+const Text = styled.div`
+  font-family: 'Cinzel';
+`
+
+const IndexPage = () => (
+  <Centered>
+    <Text>
+      <span>Time Remaining: </span>
+      <span>{formatDistance(new Date(), new Date(2018, 6, 15))}</span>
+    </Text>
+  </Centered>
+)
 
 export default IndexPage
